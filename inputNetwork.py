d669a408d40e7204b99da60c2be8bf42ecb3a630
THINK <think@ubuntu.ubuntu-domain>
2014-10-26 21:17:52 -0700

    Implemented basic input function and tested for the three test cases

---
+++ inputNetwork.py
@@ -0,0 +1,25 @@
+import json
+
+def inputNetwork(fname):
+   """Reads input json file and returns the network specification as a dictionary. 
+      The dictionary format is:
+      {
+        "Hosts" : Number of hosts, 
+        "Routers" : Number of routers,
+        "Links" : [ [Link Rate (Mbps), Link Delay (ms), Link Buffer (KB), 
+                    ['H' or 'R', id], ['H' or 'R', id] ]   
+        "Flows" : [ [Data Amt (MB), Flow start (s), Src host id, Dest host id] ]
+      }   
+
+      Host IDs range from 1 to Number of Hosts. Similarly for Flow IDs.
+      Each link corresponds to a list with the last two entries specifying which 
+      hosts ('H') or routers ('R') the link connects. 
+   """      
+
+   json_data = open(fname)
+   network_specs = json.load(json_data)
+   json_data.close()
+   return network_specs
+
+ 
+
---
+++ testInput.py
@@ -0,0 +1,13 @@
+from inputNetwork import inputNetwork
+from pprint import pprint
+
+network_specs = inputNetwork("testcase0")
+pprint(network_specs)
+print
+
+network_specs = inputNetwork("testcase1")
+pprint(network_specs)
+print
+
+network_specs = inputNetwork("testcase2")
+pprint(network_specs)
