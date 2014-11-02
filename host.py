4d9821fdf2efb7929bfd599dee64a1bfe89cf29e
adbh94 <bhagavathi94@gmail.com>
2014-11-01 18:28:35 -0700

    First host implementation.
    Hosts consist of two internal processes: monitor_outgoing_packets and
    monitor_incoming_packets. These follow a passivate/reactivate framework
    through the events send_packet and receive_packet, respectively. Note
    that the host class itself performs the reactivations of its two
    internal processes. Flows and links indirectly reactivate the internal
    host processes through the send_packet and receive_packet API,
    respectively.
    
    Is this a suitable framework for process interaction? I like this better
    than interruptions since one cannot pass objects through interruptions
    (I think). If this looks good, let's commit to this model for future
    process interactions.
    
    Tests to follow upon implementation of environment, flows, and links.

---
+++ host.py
@@ -0,0 +1,169 @@
+"""Defines the properties and methods of network host processes."""
+
+from flow import ReceivingFlow
+
+class Host(object):
+    """ 
+        A host represents an endpoint device containing multiple sending and
+        receiving flow connections.
+    
+        The host takes packets from its flows and feeds it into the link. A
+        collision results in no sent packets, though the involved flows are
+        notified. They can in turn follow a collision control protocol (e.g.,
+        Aloha method).
+    
+        The host delivers incoming packets to the corresponding flows based on
+        the flow_id parameter of the packet. It will dynamically generate
+        receiving_flows to handle new connections.
+    
+        Attributes:        
+                PROCESSING_TIME:
+                    Amount of time for host to respond to packet send
+                    requests. Primarily for implementing collision detection.
+    """
+
+    PROCESSING_TIME = 0.0000000000000001
+    
+    def __init__(self, env, host_id, link, flows):
+        """
+            Sets up a network endpoint host object.
+        
+            Args:            
+                    env:
+                        SimPy environment in which host resides.
+                    host_id:
+                        Identification number of host.
+                    link:
+                        Link object connecting host to the internet.
+                    flows:
+                        Dictionary of flows within host with flow IDs as keys.
+                        Initially, this should only consist of sending flows
+                        since receiving flows are generated on-the-fly.
+                
+        Attributes:
+                outgoing_packets:
+                    Array of outgoing packets. Collision occurs when this array
+                    has more than one packet.
+                incoming_packets:
+                    Singleton array of packet received from internet. No
+                    possibility of collision since only one link connecting
+                    host to network.
+                send_packet:
+                    Internal event triggered when a flow wants to send packet.
+                receive_packet:
+                    Internal event trigerred when packet arrives from internet.
+        """
+        
+        self.env = env
+        self.host_id = host_id
+        self.link = link
+        self.flows = flows
+        
+        # Set up packet buffers and notification events.
+        self.outgoing_packets = []
+        self.incoming_packets = []
+        self.send_packet = env.event()
+        self.receive_packet = env.event()
+
+        # Set up host monitoring of outgoing and incoming packets.
+        env.process(self.monitor_outgoing_packets(), self.env)
+        env.process(self.monitor_incoming_packets(), self.env)
+        
+    def get_host_id(self):
+        """Returns host ID."""
+        return self.host_id
+        
+    def add_sending_flow(self, sending_flow):
+        """Add sending flow to host. Usable by environment to initialize flows
+        after simulation has started."""
+        self.flows[sending_flow.get_flow_id()] = sending_flow
+        
+    def remove_flow(self, flow):
+        """Remove flow from host. It is a good convention for flows that have
+        finished sending/receiving to remove themselves from the host."""
+        del self.flows[flow.get_flow_id()]
+
+    def monitor_outgoing_packets(self, env):
+        """
+            Host passivates until an internal flow calls send_packet. Upon
+            reactivation, it waits for PROCESSING_TIME to gather all
+            simultaneous outgoing packet requests. It sends a packet if only one
+            request is issued, otherwise it notifies the corresponding hosts of
+            a collision without sending any packets.
+        """
+        
+        while True:
+            # Passivate until a flow wants to send a packet.
+            yield self.send_packet
+            
+            # Process outgoing packet requests from flows. Necessary for
+            # collision detection.
+            yield env.timeout(Host.PROCESSING_TIME)
+            
+            # Send packet into network if no collisions have occured.
+            if len(self.outgoing_packets) == 1:
+                self.link.transmit(self.outgoing_packets.pop())
+                
+            # If collision occurs, notify appropriate flows which of their
+            # packets collided without sending any packets.
+            else:
+                for packet in self.outgoing_packets:
+                    flow = self.flows[packet.get_flow_id()]
+                    flow.notify_collision(packet.get_sequence_number())
+
+                self.outgoing_packets = []
+
+            # Reset outgoing packet notification event.
+            self.send_packet = env.event()
+
+    def monitor_incoming_packets(self, env):
+        """
+            Host passivates until link calls receive_packet. Upon reactivation,
+            it attempts to forward the incoming packet to the corresponding
+            flow. If a flow does not exist to handle the incoming packet, a
+            receiving flow is generated on-the-fly.
+        """
+
+        while True:
+            # Passivate until a packet arrives from the link.
+            yield self.receive_packet
+
+            # Assuming one link per host, no collisions can occur and
+            # incoming_packets buffer necessarily has only one packet in it.
+            incoming_packet = self.incoming_packets.pop()
+            flow_id = incoming_packet.get_flow_id()
+
+            # Immediately forward incoming packet to corresponding flow, if it
+            # exists.
+            if flow_id in self.flows:
+                self.flows[flow_id].receive_packet(incoming_packet)
+    
+            # Otherwise create a new receiving flow on-the-fly. 
+            else:
+                new_receiving_flow = ReceivingFlow(flow_id)
+                self.flows[flow_id] = new_receiving_flow
+                new_receiving_flow.receive_packet(incoming_packet)
+
+            # Reset incoming packet notification event.
+            self.receive_packet = env.event()            
+
+    def send_packet(self, outgoing_packet):
+        """Method called by internal flows to send packets into the network."""
+        
+        # Add packet to outgoing_packet buffer.
+        self.outgoing_packets.append(outgoing_packet)
+
+        # Reactivate host unless collision has occured.
+        if !self.send_packet.triggered:
+            self.send_packet.succeed()
+
+        
+    def receive_packet(self, incoming_packet):
+        """Method called by link to transmit packet into the host."""
+
+        # Add packet to incoming_packet buffer.
+        self.incoming_packets.append(incoming_packet)
+
+        # Reactivate host. No possibility of collision, but check in case.
+        if !self.receive_packet.triggered:
+            self.receive_packet.succeed()        
