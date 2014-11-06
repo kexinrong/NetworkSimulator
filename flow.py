73282f9087f0a5120687528abf4d0f5713be0473
Aman <think@ubuntu.ubuntu-domain>
2014-11-05 20:22:24 -0800

    Fixed some issues that were pointed out.

--- flow.py
+++ flow.py
@@ -1,13 +1,13 @@
 """Defines the properties and methods of network flow processes."""
 
-from packet import Packet, DataPacket, AckPacket
+from packet import Packet, DataPacket, AckPacket, FINPacket
 
 class Flow(object):
     """
         A flow represents an end-to-end connection. Flows can be of two types:
         receiving and sending flows.  
     """  
-    def __init__(self, env, flow_id, hosts):
+    def __init__(self, env, flow_id, src_host, dest_host_id):
         """
             Sets up a flow object. 
  
@@ -15,25 +15,53 @@ class Flow(object):
                    env: 
                        SimPy environment in which flow resides.
                    flow_id:
-                       Identification number of flow.
-                   hosts:
-                       Tuple of two host objects. First host is the source of
-                       the flow, and the second host is the detination.
+                       ID of flow.
+                   src_host:
+                       Host object where the flow starts.
+                   dest_host_id:
+                       ID of host where flow ends.  
                
-            Attributes: same as args                        
+            Attributes: 
+                   env, flow_id, src_host, dest_host_id as above.
+                   src_host_id:
+                       ID of source host.
+                   received_packets:
+                       List of packets received so far.
+                   receive_packet:
+                       Internal event triggered when host wants to deliver 
+                       packets to the flow.                        
         """
         self.env = env
         self.flow_id = flow_id
-        self.hosts = hosts
-        
+        self.src_host = src_host
+        self.src_host_id = src_host.get_host_id()
+        self.dest_host_id = dest_host_id
+
+        # Set up received_packets buffer. May be useful for congestion control.
+        self.received_packets = []
+
+        # Set up flow's notification event
+        self.receive_packet = env.event()      
+
     def get_flow_id(self):
         """Returns flow ID."""   
         return self.flow_id
 
+    def receive_packet(self, incoming_packet):
+        """Method called by flow's source host to transmit packet to flow."""     
+        # Add packet to flow's received_packets buffer.
+        self.received_packets.append(incoming_packet)     
+
+        # Trigger notification event to reactivate flow.
+        self.receive_packet.succeed()
+
+    def end_flow(self):
+        """Remove flow from source host's list of flows."""
+        self.src_host.remove_flow(self.flow_id) 
 
 class SendingFlow(Flow):
     """
-        A sending flow sends data from the source host to detination host.
+        A sending flow sends data from the source host to destination host.
         It receives acknowledgment packets.
 
         Attributes:
@@ -47,7 +75,7 @@ class SendingFlow(Flow):
     DATA_PCK_SIZE = 1024
     FIN_PCK_SIZE = 64
 
-    def __init__(self, env, flow_id, hosts, data_amt_MB, start_time_s):
+    def __init__(self, env, flow_id, src_host, dest_host_id, data_amt_MB, start_time_s):
         """
             Sets up a sending flow object.
            
@@ -56,9 +84,10 @@ class SendingFlow(Flow):
                        SimPy environment in which flow resides.
                    flow_id:
                        Identification number of flow.
-                   hosts:
-                       Tuple of two host objects. First host is the source of
-                       the flow, and the second host is the detination.
+                   src_host:
+                       Host object in which the flow starts.
+                   dest_host_id:
+                       ID of host where flow ends. 
                    data_amt_MB:
                        Amount of data to be transferred in MB. Input uses MB.
                    start_time_s:
@@ -66,7 +95,9 @@ class SendingFlow(Flow):
                        data. Input uses seconds.
                
             Attributes:
-                   env, flow_id and hosts as above.
+                   env, flow_id, src_host and dest_host_id as above.
+                   src_host_id:
+                       ID of source host.
                    data_amt: 
                        Amount of data in bytes the flow needs to send. Bytes 
                        are more convenient to work with since packet sizes are
@@ -74,23 +105,17 @@ class SendingFlow(Flow):
                    start_time: 
                        Time in ms after which flow starts transmitting data. 
                        This is convenient because the environment uses ms.
-                   received_acks:
-                       List of ack packets received so far.
-                   receive_ack:
-                       Internal event triggered when host wants to deliver 
-                       ack packet to its sending flow.
+                   received_packets:
+                       List of packets received so far.
+                   receive_packet:
+                       Internal event triggered when host wants to deliver a 
+                       packet to the flow.
         """       
-        super(SendingFlow, self).__init__(env, flow_id, hosts)
+        super(SendingFlow, self).__init__(env, flow_id, src_host, dest_host_id)
 
         self.data_amt = data_amt_MB * MB_TO_BYTES
         self.start_time = start_time_s * S_TO_MS
 
-        # Set up ack_packet buffer. May be useful for congestion control.
-        self.received_acks = []
-
-        # Set up flow's notification event
-        self.receive_ack = env.event()
-        
         # Add the run generator to the event queue.
         env.process(self.run())
 
@@ -103,43 +128,41 @@ class SendingFlow(Flow):
            Data transmission gets stuck if an acknowledment is not received.
        """
        # Passivate until start time.
-       yield env.timeout(self.start_time)
+       yield self.env.timeout(self.start_time)
 
        seq_num = 1
        while self.data_amt > 0:
            # Create new packet.
-           data_packet = DataPacket(self.hosts[0], self.flow_id, self.hosts[1], 
-                                    self.env.now, DATA_PCK_SIZE, seq_num)
+           data_packet = DataPacket(self.src_host_id, self.flow_id, 
+                                    self.dest_host_id, self.env.now, 
+                                    DATA_PCK_SIZE, seq_num)
   
            # Call host function to send packet.  
-           self.hosts[0].send_packet(data_packet)
+           self.src_host.send_packet(data_packet)
 
-           # Passivate until an acknowledgment is received.   
-           yield self.receive_ack 
-                      
+           # Passivate until an ack packet is received.   
+           yield self.receive_packet 
+           received_packet = self.received_packets[-1]
+           
+           # Throw an error if received packet is not an ack packet.
+           assert(received_packet.get_packet_type() == 
+                  Packet.PacketTypes.acknowledgement_packet)           
+
            # Check if received ack packet has correct seq_num.  
-           if (self.received_acks[-1].get_sequence_number() == seq_num):
+           if (received_packet.get_sequence_number() == seq_num):
                # Update data_amt and seq_num.
                self.data_amt -= DATA_PCK_SIZE
                seq_num += 1
 
            # Reset event
-           self.receive_ack = env.event()
+           self.receive_packet = self.env.event()
        
        # All the data has been sent. Send FIN packet.
-       fin_packet = FINPacket(self.hosts[0], self.flow_id, self.hosts[1], 
-                              self.env.now, FIN_PCK_SIZE, seq_num)
-       self.hosts[0].send_packet(fin_packet) 
-       
-           
-    def receive_packet(self, incoming_ack):
-       """Method called by flow's source host to transmit ack packet to flow."""     
-       # Add packet to flow's received_acks buffer.
-       self.received_acks.append(incoming_ack)     
-
-       # Trigger notification event to reactivate flow.
-       self.receive_ack.succeed()
-
+       fin_packet = FINPacket(self.src_host_id, self.flow_id, 
+                              self.dest_host_id, self.env.now, 
+                              FIN_PCK_SIZE, seq_num)
+       self.src_host.send_packet(fin_packet) 
+       self.end_flow()      
 
 class ReceivingFlow(Flow):
     """
@@ -160,28 +183,26 @@ class ReceivingFlow(Flow):
                    flow_id:
                        Identification number of flow (same as corresponding 
                        sending flow).
-                   hosts:
-                       Tuple of two host objects. First host is the source of
-                       the flow, and the second host is the detination.
+                   src_host:
+                       Host object in which the flow starts.
+                   dest_host_id:
+                       ID of host where flow ends. 
                
             Attributes:
-                   env, flow_id and hosts as above.
+                   env, flow_id, src_host and dest_host_id as above.
+                   src_host_id:
+                       ID of source host.
                    received_packets:
                        List of packets received so far.
                    receive_packet:
-                       Internal event triggered when host wants to deliver 
-                       data/fin packet to its receiving flow.
+                       Internal event triggered when host wants to deliver a 
+                       packet to the flow.
+
         """       
         super(ReceivingFlow, self).__init__(env, flow_id, hosts)
 
-        # Set up received packets buffer. May be useful in congestion control.
-        self.received_packets = []
-
-        # Set up flow's notification event
-        self.receive_packet = env.event()
-        
         # Add the run generator to the event queue.
-        env.process(self.run())
+        self.env.process(self.run())
 
     def run(self):
        """
@@ -197,24 +218,21 @@ class ReceivingFlow(Flow):
            received_packet = self.received_packets[-1]
            if (received_packet.get_packet_type() == Packet.PacketTypes.data_packet):
                # Create new ack packet with same seq_num as received data packet.
-               ack_packet = AckPacket(self.hosts[0], self.flow_id, self.hosts[1], 
-                                      self.env.now, ACK_PCK_SIZE, 
-                                      received_packet.get_sequence_number())
+               ack_packet = AckPacket(self.src_host_id, self.flow_id, 
+                                      self.dest_host_id, self.env.now, 
+                                      ACK_PCK_SIZE, received_packet.get_sequence_number())
      
                # Call host function to send packet.  
-               self.hosts[0].send_packet(ack_packet)
+               self.src_host.send_packet(ack_packet)
 
                # Reset event.
-               self.receive_packet = env.event()
+               self.receive_packet = self.env.event()
 
            else: 
+               # Throw error if packet is not a FIN_packet
+               assert(received_packet.get_packet_type() == 
+                      Packet.PacketTypes.fin_packet)
                # FIN_packet received. Stop running.
                break     
-          
-    def receive_packet(self, incoming_packet):
-       """Method called by flow's source host to transmit data/fin packet to flow."""     
-       # Add packet to flow's received_packets buffer.
-       self.received_packets.append(incoming_packet)     
-
-       # Trigger notification event to reactivate flow.
-       self.receive_packet.succeed()   
+       
+       self.end_flow()
