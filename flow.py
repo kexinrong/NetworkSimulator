f1088a3a98aff934f17fa3e8fd367cc90ff576b0
Aman <think@ubuntu.ubuntu-domain>
2014-11-09 13:34:10 -0800

    Added stats collection
    Only sending flows report metrics: send/receive rate and RTT.
    I have used an internal interval_start_time instead of env.interval
    because the flow also needs to account for its start/end times
    when averaging metrics.
    
    I have assumed the same dictionary format as in host.py.
    
    I am not sure what the flow should report if the report function
    is called before its start time. Perhaps the report function should
    be called on the flow by the main env only after its start time?

--- flow.py
+++ flow.py
@@ -72,10 +72,12 @@ class SendingFlow(Flow):
         Attributes:
                      MB_TO_BYTES: Conversion factor.  
                      S_TO_MS: Conversion factor.
+                     MS_TO_S: Conversion factor.
                      DATA_PCK_SIZE: Size of data packet in bytes.
     """
     MB_TO_BYTES = 2 ** 20
     S_TO_MS = 1000
+    MS_TO_S = 0.001
     DATA_PCK_SIZE = 1024
 
     def __init__(self, env, flow_id, src_host, dest_host_id, data_amt_MB, start_time_s):
@@ -108,66 +110,126 @@ class SendingFlow(Flow):
                    start_time: 
                        Time in ms after which flow starts transmitting data. 
                        This is convenient because the environment uses ms.
+                   end_time:
+                       Time in ms when flow is done transmitting data. Needed 
+                       for data reporting.
                    received_packets:
                        List of packets received so far.
                    receive_packet:
                        Internal event triggered when host wants to deliver a 
                        packet to the flow.
+                   interval_start_time:
+                       Time of start of flow/last time report() was called.
+                   num_packets_sent:
+                       Number of packets sent since interval_start_time.
+                   num_packets_received:
+                       Number of packets received since interval_start_time.
+                   sum_RTT_delay:
+                       Sum of round-trip time delays seen for ack packets 
+                       received since interval_start_time.                             
         """       
         super(SendingFlow, self).__init__(env, flow_id, src_host, dest_host_id)
 
         self.data_amt = data_amt_MB * MB_TO_BYTES
         self.start_time = start_time_s * S_TO_MS
+        # -1 means flow has not ended yet.
+        self.end_time = -1
+
+        # Initiliaze fields for metrics reporting.
+        self.interval_start_time = self.start_time
+        self.num_packets_sent = 0
+        self.num_packets_received = 0
+        self.sum_RTT_delay = 0
 
         # Add the run generator to the event queue.
         env.process(self.run(env))
 
     def run(self, env):
-       """
-           Implements the functionality of a sending flow.
+        """
+            Implements the functionality of a sending flow.
+ 
+            In this implementation, the flow sends a packet, waits for acknowledgment
+            and then sends another packet until there is no more data to send. 
+            Data transmission gets stuck if an acknowledment is not received.
+        """
+        # Passivate until start time.
+        yield env.timeout(self.start_time)
 
-           In this implementation, the flow sends a packet, waits for acknowledgment
-           and then sends another packet until there is no more data to send. 
-           Data transmission gets stuck if an acknowledment is not received.
-       """
-       # Passivate until start time.
-       yield env.timeout(self.start_time)
-
-       seq_num = 1
-       while self.data_amt > 0:
-           # Create new packet.
-           data_packet = DataPacket(self.src_host_id, self.flow_id, 
-                                    self.dest_host_id, env.now, 
-                                    seq_num)
+        seq_num = 1
+        while self.data_amt > 0:
+            # Create new packet.
+            data_packet = DataPacket(self.src_host_id, self.flow_id, 
+                                     self.dest_host_id, env.now, 
+                                     seq_num)
   
-           # Call host function to send packet.  
-           self.src_host.send_packet(data_packet)
+            # Call host function to send packet, update counter.  
+            self.src_host.send_packet(data_packet)
+            self.num_packets_sent += 1
 
-           # Passivate until an ack packet is received.   
-           yield self.receive_packet 
-           received_packet = self.received_packets[-1]
+            # Passivate until an ack packet is received, update counter.   
+            yield self.receive_packet 
+            self.num_packets_received += 1
+     
+            received_packet = self.received_packets[-1]
            
-           # Throw an error if received packet is not an ack packet.
-           assert(received_packet.get_packet_type() == 
-                  Packet.PacketTypes.acknowledgement_packet)           
+            # Throw an error if received packet is not an ack packet.
+            assert(received_packet.get_packet_type() == 
+                   Packet.PacketTypes.acknowledgement_packet)           
 
-           # Check if received ack packet has correct seq_num.  
-           if (received_packet.get_sequence_number() == seq_num):
-               # Update data_amt and seq_num.
-               self.data_amt -= DATA_PCK_SIZE
-               seq_num += 1
+            # Check if received ack packet has correct seq_num.  
+            if (received_packet.get_sequence_number() == seq_num):
+                # Update data_amt and seq_num.
+                self.data_amt -= DATA_PCK_SIZE
+                seq_num += 1
+                # Add RTT delay for data_packet.
+                self.sum_RTT_delay += (env.now - data_packet.get_timestamp())
 
-           # Reset event
-           self.receive_packet = env.event()
+            # Reset event
+            self.receive_packet = env.event()
        
-       # All the data has been sent. Send FIN packet.
-       fin_packet = FINPacket(self.src_host_id, self.flow_id, 
-                              self.dest_host_id, env.now, 
-                              seq_num)
-       self.src_host.send_packet(fin_packet) 
-       # End flow.
-       self.end_flow()      
+        # All the data has been sent. Send FIN packet.
+        fin_packet = FINPacket(self.src_host_id, self.flow_id, 
+                               self.dest_host_id, env.now, 
+                               seq_num)
+        self.src_host.send_packet(fin_packet) 
+        self.num_packets_sent += 1
+        # End flow.
+        self.end_time = env.now
+        self.end_flow()   
 
+    def get_reporting_interval():
+        """Calculates the appropriate interval (in s) over which reporting 
+           is done."""
+        # If flow has ended, then the time_interval ends at end_time. 
+        if (self.end_time > 0):
+           time_interval = (self.end_time - self.interval_start_time) * MS_TO_S
+        else:
+           time_interval = (self.env.now - self.interval_start_time) * MS_TO_S
+       
+        return time_interval 
+
+    def report(self):
+        """Report average flow send/receive rate (in packets/s) and average RTT
+           delay (in ms) since start of flow/last time report was called."""
+       
+        # Time passed in s.
+        time_interval = get_reporting_interval()
+       
+        # Calculate send/receive rates (packets/s) and average RTT (in ms).
+        flow_send_rate = self.num_packets_sent / time_interval
+        flow_receive_rate = self.num_packets_received / time_interval
+        flow_avg_RTT = self.sum_RTT_delay / self.num_packets_received
+
+        # Reset counters.
+        self.interval_start_time = self.env.now
+        self.num_packets_sent = 0
+        self.num_packets_received = 0
+        self.sum_RTT_delay = 0
+
+        return {self.env.Measurements.flow_send_rate : flow_send_rate,
+                self.env.Measurements.flow_receive_rate : flow_receive_rate,
+                self.env.Measurements.flow_avg_RTT : flow_avg_RTT}
+               
 class ReceivingFlow(Flow):
     """
         A receiving flow receives data packets and sends acknowledgments.
@@ -207,34 +269,34 @@ class ReceivingFlow(Flow):
         self.env.process(self.run(env))
 
     def run(self, env):
-       """
-           Implements the functionality of a receiving flow.
+        """
+            Implements the functionality of a receiving flow.
 
-           In this implementation, the flow simply sends an ACK packet upon
-           receiving a data packet. 
-       """
-       while True:
-           # Passivate until packet received.
-           yield self.receive_packet
-           
-           received_packet = self.received_packets[-1]
-           if (received_packet.get_packet_type() == Packet.PacketTypes.data_packet):
-               # Create new ack packet with same seq_num as received data packet.
-               ack_packet = AckPacket(self.src_host_id, self.flow_id, 
-                                      self.dest_host_id, env.now, 
-                                      received_packet.get_sequence_number())
-     
-               # Call host function to send packet.  
-               self.src_host.send_packet(ack_packet)
+            In this implementation, the flow simply sends an ACK packet upon
+            receiving a data packet. 
+        """
+        while True:
+             # Passivate until packet received.
+            yield self.receive_packet
+            
+            received_packet = self.received_packets[-1]
+            if (received_packet.get_packet_type() == Packet.PacketTypes.data_packet):
+                # Create new ack packet with same seq_num as received data packet.
+                ack_packet = AckPacket(self.src_host_id, self.flow_id, 
+                                       self.dest_host_id, env.now, 
+                                       received_packet.get_sequence_number())
+      
+                # Call host function to send packet.  
+                self.src_host.send_packet(ack_packet)
 
-               # Reset event.
-               self.receive_packet = env.event()
+                # Reset event.
+                self.receive_packet = env.event()
 
-           else: 
-               # Throw error if packet is not a FIN_packet
-               assert(received_packet.get_packet_type() == 
-                      Packet.PacketTypes.fin_packet)
-               # FIN_packet received. Stop running.
-               break     
+            else: 
+                # Throw error if packet is not a FIN_packet
+                assert(received_packet.get_packet_type() == 
+                       Packet.PacketTypes.fin_packet)
+                # FIN_packet received. Stop running.
+                break     
        
-       self.end_flow()
+        self.end_flow()
